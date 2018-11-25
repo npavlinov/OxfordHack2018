9ba0eeb3643af9578a7538c8716a74af52a5441c
Paul Dubois <pauldubois98@hotmail.com>
2018-11-25 05:43:30 +0100

    compiler 1st working

--- back/test.py
+++ back/test.py
@@ -1,9 +1,11 @@
-import requests
-import json
 import sys
+import compiler
 
+if __name__=="__main__":
 
-text1='hello i guess'
-text2="hello i guess"
+    if len(sys.argv)==1:
+        files_names = ['first.txt', 'second.txt','parietal_lobe.txt']
+    else:
+        files_names = sys.argv[1:]
 
-import API
+    compiler.compile(files_names, "out.md")
